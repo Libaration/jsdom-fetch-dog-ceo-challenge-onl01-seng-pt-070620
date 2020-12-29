19031612ea6d2935026f5d9b37ff870b2c0bbe73
Cristian Deleon <libaration@gmail.com>
2020-12-28 22:46:05 -0500

    commit tree 82616e0f50e7d4ee6cc0850a3e47ef2934ab4450

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const renderImages = images => {
+    const imagesList = document.getElementById('dog-image-container')
+    images.forEach(image => {
+        const img = document.createElement('img');
+        img.setAttribute('src', `${image}`);
+        img.setAttribute('width', '200');
+        imagesList.append(img);
+    });
+};
+
+const renderBreeds = breeds => {
+
+    const breedsList = document.getElementById('dog-breeds')
+    for(let [key,value] of Object.entries(breeds)){
+        const li = document.createElement('li');
+        li.innerHTML = `${key}`
+        breedsList.appendChild(li)
+
+        if(value.length !== 0){
+            const subBreed = document.createElement('li')
+            const subList = document.createElement('ul')
+            breedsList.appendChild(subList)
+            subBreed.innerHTML = `${value}`
+            subList.appendChild(subBreed)
+        }
+    }
+};
+
+const fetchImages = url => {
+    fetch(url)
+        .then((r) => r.json())
+        .then(images => renderImages(images.message))
+};
+const fetchBreeds = url => {
+    fetch(url)
+        .then((r) => r.json())
+        .then(breeds => renderBreeds(breeds.message))
+};
+document.addEventListener('DOMContentLoaded', () =>{
+    fetchImages('https://dog.ceo/api/breeds/image/random/4');
+    fetchBreeds('https://dog.ceo/api/breeds/list/all');
+});
